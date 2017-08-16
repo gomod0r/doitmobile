0c2a9ed7e411ce22ebba51ae3c31c44723715125
Hakan Şahin <hakan@Kenans-iMac.local>
2017-08-16 10:19:01 +0300

    taskdetail sayfası yapıldı.

--- src/components/taskdetail.js
+++ src/components/taskdetail.js
@@ -4,6 +4,7 @@ import {
     View,
     Text,
     StatusBar,
+    TouchableHighlight,
     TouchableWithoutFeedback
 } from 'react-native';
 
@@ -28,19 +29,26 @@ export default class TaskDetail extends Component {
                 <Text style={{ marginTop:18, marginLeft:16, color:'#fb684b', fontSize:30, fontWeight:'500' }}> Görev Adı </Text>
                 <View style={{ height:189, paddingHorizontal:16, marginTop:25 }}>
                     <Card style={{ borderRadius:7 }}>
-                        <View style={{ borderBottomWidth:1, borderBottomColor:'#d8d8d8', height:94 }}>
-                            <View style={{ flexDirection:'row', marginTop:15, marginLeft:16 }}>
-                                <Icon name="date-range" size={25} style={{ color:'#7200da' }} />
-                                <Text style={{ marginLeft:16, fontSize:18, color:'#9b9b9b' }}>Tarih Seç</Text>
+                        <View style={{ borderBottomWidth:1, borderBottomColor:'#d8d8d8', height:94, flexDirection:'row' }}>
+                            <View style={{ flexDirection:'column', flex:1 }}>
+                                <View style={{ flexDirection:'row', marginTop:15, marginLeft:16 }}>
+                                    <Icon name="date-range" size={25} style={{ color:'#7200da' }} />
+                                    <Text style={{ marginLeft:16, fontSize:18, color:'#9b9b9b' }}>Tarih Seç</Text>
+                                </View>
+                                <View style={{ flexDirection:'row', marginTop:15, marginLeft:16 }}>
+                                    <MaterialCommunityIcons name="clock" size={25} style={{ color:'#7200da' }} />
+                                    <Text style={{ marginLeft:16, fontSize:18, color:'#9b9b9b' }}>Saat Seç</Text>
+                                </View>
                             </View>
-                            <View style={{ flexDirection:'row', marginTop:15, marginLeft:16 }}>
-                                <MaterialCommunityIcons name="clock" size={25} style={{ color:'#7200da' }} />
-                                <Text style={{ marginLeft:16, fontSize:18, color:'#9b9b9b' }}>Saat Seç</Text>
+                            <View style={{ justifyContent:'center', alignItems:'flex-end', flex:1, marginRight:16 }}>
+                                <TouchableHighlight style={{ width:32, alignItems:'center', justifyContent:'center',borderRadius:16, height:32, borderWidth:1, borderColor:'#d8d8d8' }}>
+                                    <Icon size={20} name='notifications-active' style={{ color:'#75d701' }}/>
+                                </TouchableHighlight>
                             </View>
                         </View>
-                        <View style={{ marginTop:16, marginLeft:16 }}>
-                            <Text style={{ fontSize:18, color:'#9b9b9b' }}>Not Ekle</Text>
-                            <View style={{ flexDirection:'row', justifyContent:'flex-end', marginRight:16, alignItems:'flex-end' }}>
+                        <View style={{ flex:1, marginTop:16, marginLeft:16, flexDirection:'row' }}>
+                            <Text style={{ fontSize:18, color:'#9b9b9b', flex:1 }}>Not Ekle</Text>
+                            <View style={{ flexDirection:'row', justifyContent:'flex-end', flex:1, marginRight:16, alignItems:'flex-end', marginBottom:16 }}>
                                 <TouchableWithoutFeedback hitSlop={{ top:20 }}>
                                     <MaterialCommunityIcons name="pencil" style={{ fontSize:15, color:'#2e70be' }}/>
                                 </TouchableWithoutFeedback>
@@ -51,6 +59,9 @@ export default class TaskDetail extends Component {
                         </View>
                     </Card>
                 </View>
+                <TouchableHighlight style={{ marginTop:40, width:30, height:30, borderRadius:15, alignSelf:'center', backgroundColor:'#7200da', alignItems:'center', justifyContent:'center' }}>
+                    <Icon name='add' style={{ color:'white' }} size={25} />
+                </TouchableHighlight>
             </View>
         );
     }
